486e4a9416bd4d970be73f35c8e98caa74d509e2
Marco Túlio Pimenta Gontijo <marcotmarcot@gmail.com>
2012-09-28 19:38:12 -0300

    Working on submit.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,7 +1,27 @@
 {-# LANGUAGE TupleSections, OverloadedStrings #-}
 module Handler.Home where
 
+-- base
+import Control.Exception (evaluate)
+import System.Timeout (timeout)
+import Control.Monad (when)
+
+-- time
+import Data.Time (getCurrentTime, utcToZonedTime, TimeZone (TimeZone))
+
+-- text
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+-- deepseq
+import Control.DeepSeq (force)
+
+-- scilab
+import Scilab.Interpreter
+
+-- mini-scilab-site
 import Import
+import Exercises
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -37,3 +57,39 @@ sampleForm :: Form (FileInfo, Text)
 sampleForm = renderDivs $ (,)
     <$> fileAFormReq "Choose a file"
     <*> areq textField "What's on the file?" Nothing
+
+getSubmitR :: Handler RepHtml
+getSubmitR = defaultLayout $(widgetFile "submit")
+
+postSubmitR :: Handler RepHtml
+postSubmitR
+  = do
+    student <- runInputPost $ ireq textField "student"
+    exercise <- T.unpack <$> runInputPost (ireq textField "exercise")
+    code <- runInputPost $ ireq textField "code"
+    result <- liftIO $ checkExercises (io $ read exercise) code
+    when result $ liftIO $ success student exercise
+    let
+      message = if result then "Correto!" else "Erro!" :: Text
+      -- doits = show doit
+    defaultLayout $(widgetFile "posted")
+
+checkExercises :: ExerciseIO -> Text -> IO Bool
+checkExercises (rea_inp, exp_out) code
+  = do
+    result <- timeout 100000 $ evaluate $ force $ interpret rea_inp code
+    return
+      $ case result of
+        Nothing -> False
+        Just (lef_inp, rea_out) -> lef_inp == [] && rea_out == exp_out
+
+success :: T.Text -> String -> IO ()
+success student exercise
+  = do
+    time
+      <- T.pack
+        <$> show
+        <$> utcToZonedTime (TimeZone (-180) False "BRT")
+        <$> getCurrentTime
+    T.appendFile ("/home/marcot/mini-scilab-site/" ++ exercise)
+      $ time <> ": " <> student <> "\n"
